0a338eceaff2e96e17c32db2eda2eeb36d279543
jiyuujin <jiyuujinunite@gmail.com>
2021-08-14 10:19:27 +0900

    load manifest.json

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -12,6 +12,7 @@ const MyApp = ({ Component, pageProps }) => {
           name="viewport"
           content="width=device-width, initial-scale=1, shrink-to-fit=no"
         />
+        <link rel="manifest" href="/manifest.json" />
       </Head>
       <ColorThemeProvider>
         <Component {...pageProps} />
