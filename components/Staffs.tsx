20e89a24109763ca142c50c4e3a7c2bc5495c173
jiyuujin <jiyuujinunite@gmail.com>
2021-08-26 08:11:13 +0900

    fix: staff style on SP

--- components/Staffs.tsx
+++ components/Staffs.tsx
@@ -1,21 +1,19 @@
 import { FC } from 'react'
 // import Image from 'next/image'
-import styles from '../static/Section.module.scss'
+import styles from '../static/Staff.module.scss'
 import { members } from '../contents/members'
 
 const Staffs: FC = () => {
   return (
     <section id={'staffs'} className={styles.section}>
       <h2>{'Staffs'}</h2>
-      <div className={styles.detail}>
+      <div className={styles.staffs}>
         {members.map((member) => {
           return (
             <div key={member.name} className={styles.icon}>
               <img
                 alt={`Image ${member.name}`}
                 src={`icons/${member.iconFilename}`}
-                width={120}
-                height={120}
               />
               <div className={styles.info}>
                 <div className={styles.name}>{member.name}</div>
