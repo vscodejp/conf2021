860850994d894dac6938deac97e57b01a1678b67
jiyuujin <jiyuujinunite@gmail.com>
2021-08-30 16:08:22 +0900

    feat: concept image

---
+++ components/Concept.tsx
@@ -0,0 +1,13 @@
+import { FC } from 'react'
+import Image from 'next/image'
+import styles from '../static/Concept.module.scss'
+
+const Concept: FC = () => {
+  return (
+    <div id={'concept'} className={styles.concept}>
+      <Image src={'/concept.png'} width={1300} height={683} />
+    </div>
+  )
+}
+
+export default Concept
