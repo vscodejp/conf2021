5f6246393bab7ca9e8232237d14764b9c6ed1717
jiyuujin <jiyuujinunite@gmail.com>
2021-08-30 18:18:03 +0900

    feat: eventInfo

--- components/About.tsx
+++ components/About.tsx
@@ -8,7 +8,7 @@ const About: FC = () => {
       <div className={styles.detail}>
         <dl>
           <dt>開催日時</dt>
-          <dd>2021年 XX月 XX日</dd>
+          <dd>2021年 11月 20日</dd>
         </dl>
         <dl>
           <dt>会場</dt>
@@ -16,7 +16,20 @@ const About: FC = () => {
         </dl>
         <dl>
           <dt>参加費</dt>
-          <dd>後日公表予定</dd>
+          <dd>無料</dd>
+        </dl>
+        <dl>
+          <dt>参加方法</dt>
+          <dd>
+            <a
+              href="https://vscode.connpass.com/event/221961/"
+              aria-label="connpass フォームより参加を申し込む"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              connpass フォームより参加を申し込む
+            </a>
+          </dd>
         </dl>
       </div>
     </section>
