0798781817e7aa1002f8c6c7e379b9e96a2634c0
jiyuujin <jiyuujinunite@gmail.com>
2021-08-30 18:05:06 +0900

    fix: urlPath

--- components/Concept.tsx
+++ components/Concept.tsx
@@ -2,10 +2,12 @@ import { FC } from 'react'
 // import Image from 'next/image'
 import styles from '../static/Concept.module.scss'
 
+const urlPrefix = process.env.NODE_ENV === 'production' ? '/conf2021' : ''
+
 const Concept: FC = () => {
   return (
     <div id={'concept'} className={styles.concept}>
-      <img alt="conference concept" src={'/concept.png'} />
+      <img alt="conference concept" src={`${urlPrefix}/concept.png`} />
     </div>
   )
 }
