83dc6493ea5cc30eea07142da83942bd8014c9aa
jiyuujin <jiyuujinunite@gmail.com>
2021-08-30 19:52:11 +0900

    fix: lint

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -13,7 +13,12 @@ export default function Home() {
         <h1 className={styles.title}>{'問い合わせ'}</h1>
 
         <div className={styles.content}>
-          <ButtonElement path={'https://docs.google.com/forms/d/e/1FAIpQLSeDsAlBwncykjmyia0Z-7W-1IMIPHBGZisRZJT_KZ8J8y6rRQ/viewform'} isExternalLink>
+          <ButtonElement
+            path={
+              'https://docs.google.com/forms/d/e/1FAIpQLSeDsAlBwncykjmyia0Z-7W-1IMIPHBGZisRZJT_KZ8J8y6rRQ/viewform'
+            }
+            isExternalLink
+          >
             {'Googleフォームはこちら'}
           </ButtonElement>
         </div>
