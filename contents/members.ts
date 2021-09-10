0a6965b3c6437a606808c53f281d64640195f73a
jiyuujin <jiyuujinunite@gmail.com>
2021-09-11 08:14:29 +0900

    add staffs

--- contents/members.ts
+++ contents/members.ts
@@ -4,4 +4,34 @@ export const members = [
     url: 'https://twitter.com/jiyuujinlab',
     iconFilename: 'bakeneko2.png',
   },
+  {
+    name: 'setoazu',
+    url: 'https://twitter.com/setoazusa',
+    iconName: 'https://pbs.twimg.com/profile_images/1392132106449420288/eF0s_kDi_400x400.jpg',
+  },
+  {
+    name: 'Nori Suzuki',
+    url: 'https://twitter.com/szkn27',
+    iconName: 'https://pbs.twimg.com/profile_images/1362573976711561216/qAxHDZyi_400x400.jpg',
+  },
+  {
+    name: 'Atsushi Morimoto',
+    url: 'https://twitter.com/74th',
+    iconName: 'https://pbs.twimg.com/profile_images/1296047865798696963/42BVehEz_400x400.jpg',
+  },
+  {
+    name: 'Aya Tokura',
+    url: 'https://twitter.com/ayatokura',
+    iconName: 'https://pbs.twimg.com/profile_images/1379041781623390213/SvPXdZwE_400x400.jpg',
+  },
+  {
+    name: 'ふじい',
+    url: 'https://twitter.com/dafujii_k',
+    iconName: 'https://pbs.twimg.com/profile_images/1300633615299084290/2I55c5_2_400x400.jpg',
+  },
+  {
+    name: 'ロフトくん',
+    url: 'https://twitter.com/loftkun',
+    iconName: 'https://pbs.twimg.com/profile_images/1130041560471486464/2vwZ3x9u_400x400.png',
+  },
 ]
