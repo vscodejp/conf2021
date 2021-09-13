853965a29f6c9133e409ec227250032b36824557
jiyuujin <jiyuujinunite@gmail.com>
2021-09-13 21:12:09 +0900

    fix: urlPath

--- components/Header.tsx
+++ components/Header.tsx
@@ -6,25 +6,27 @@ import { conferenceName } from '../utils/constants'
 
 import { ColorThemeSwitch } from './ColorThemeSwitch'
 
+const urlPrefix = process.env.NODE_ENV === 'production' ? '/conf2021' : ''
+
 const Header: FC = () => {
   const lang = i18next.language.substring(0, 2)
   return (
     <div className={styles.header}>
-      <Link href={`${lang}/`}>
+      <Link href={`${urlPrefix}/${lang}/`}>
         <a className={styles.logo}>{conferenceName}</a>
       </Link>
       <div className={styles.linksWrapper}>
         <nav className={styles.links}>
-          <Link href={`${lang}/#about`}>
+          <Link href={`${urlPrefix}/${lang}/#about`}>
             <a className={styles.link}>{i18next.t('about')}</a>
           </Link>
-          <Link href={`${lang}/#speakers`}>
+          <Link href={`${urlPrefix}/${lang}/#speakers`}>
             <a className={styles.link}>{i18next.t('speakers')}</a>
           </Link>
-          <Link href={`${lang}/#timetable`}>
+          <Link href={`${urlPrefix}/${lang}/#timetable`}>
             <a className={styles.link}>{i18next.t('timetable')}</a>
           </Link>
-          <Link href={`${lang}/#staffs`}>
+          <Link href={`${urlPrefix}/${lang}/#staffs`}>
             <a className={styles.link}>{i18next.t('staffs')}</a>
           </Link>
           <a href="#" className={styles.link}>
