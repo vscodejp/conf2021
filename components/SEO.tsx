fe5ab5efd4a07d1cf5424f6100a8846cca05f809
jiyuujin <jiyuujinunite@gmail.com>
2021-09-17 18:16:07 +0900

    fix: seo at rootPath

--- components/SEO.tsx
+++ components/SEO.tsx
@@ -1,3 +1,4 @@
+import { useRouter } from 'next/router'
 import Head from 'next/head'
 import i18next from 'i18next'
 import { conferenceName, description } from '../utils/constants'
@@ -5,6 +6,8 @@ import { conferenceName, description } from '../utils/constants'
 const urlPrefix = process.env.NODE_ENV === 'production' ? 'https://vscodejp.github.io/conf2021' : ''
 
 const SEO = () => {
+  const router = useRouter()
+  const { pathname } = router
   return (
     <>
       <Head>
@@ -12,14 +15,23 @@ const SEO = () => {
         <link rel="icon" href={`${urlPrefix}/favicon.ico`} />
         <link rel="manifest" href={`${urlPrefix}/manifest.json`} />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta property="description" content={i18next.t('og_description') || description} />
+        <meta
+          property="description"
+          content={pathname !== '/' ? i18next.t('og_description') : description}
+        />
         <meta property="og:site_name" content={conferenceName} />
         <meta property="og:type" content="website" />
-        <meta property="og:description" content={i18next.t('og_description') || description} />
+        <meta
+          property="og:description"
+          content={pathname !== '/' ? i18next.t('og_description') : description}
+        />
         <meta property="og:title" content={conferenceName} />
         <meta property="og:url" content={conferenceName} />
         <meta property="og:image" content={`${urlPrefix}/concept.png`} />
-        <meta property="twitter:description" content={i18next.t('og_description') || description} />
+        <meta
+          property="twitter:description"
+          content={pathname !== '/' ? i18next.t('og_description') : description}
+        />
         <meta property="twitter:title" content={conferenceName} />
         <meta property="twitter:card" content="summary_large_image" />
         <meta property="twitter:site" content="@vscodejp" />
