a95115d153226eb958ca32ff38a101d7e17b215e
jiyuujin <jiyuujin@users.noreply.github.com>
2022-12-05 07:43:56 +0000

    Automated deployment: Mon Dec  5 07:43:56 UTC 2022 ebb7f506a240ff64e41a5db83d1416b5ffa86be8

---
+++ _next/static/57NUbs505w0saWdLYWAkx/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002F[lang]","\u002F[lang]\u002Fcode-for-conduct","\u002F[lang]\u002Fcontact","\u002F[lang]\u002Fprivacy-policy"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
