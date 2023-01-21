98a028572267e2a39abe2ce6c1fd773396d78026
yuma-kitamura <jiyuujinunite@gmail.com>
2023-01-21 11:42:18 +0900

    refactor: all codes

--- src/components/PlayerSection.tsx
+++ src/components/PlayerSection.tsx
@@ -1,34 +1,38 @@
 import { FC } from 'root/react-app-env'
-import dynamic from 'next/dynamic'
-import i18next from 'i18next'
-import styles from '@static/Home.module.scss'
-import playerStyles from '@static/Player.module.scss'
-
-// https://github.com/cookpete/react-player/issues/1474#issuecomment-1184645105
-const ReactPlayer = dynamic(() => import('react-player/lazy'), { ssr: false })
-
-export const PreEventPlayer: FC = () => {
-  return (
-    <div id={'player'} className={playerStyles.player}>
-      <h2 className={styles.title}>{i18next.t('pre_event')}</h2>
-      <p>{i18next.t('subscribing_to_the_youtube_channel')}</p>
-      <ReactPlayer width={'100%'} url="https://youtu.be/Y8dl1y2qdG8" />
-    </div>
-  )
-}
+import { t } from 'i18next'
+import styles from '@static/Player.module.scss'
 
 export const MainEventPlayer: FC = () => {
   return (
-    <div id={'player'} className={playerStyles.player}>
-      <h2>{'Handson'}</h2>
-      <p>{i18next.t('subscribing_to_the_youtube_channel')}</p>
-      <ReactPlayer width={'100%'} url="https://youtu.be/wmjX6-mDsME" />
-      <h2>{'Track A'}</h2>
-      <p>{i18next.t('subscribing_to_the_youtube_channel')}</p>
-      <ReactPlayer width={'100%'} url="https://youtu.be/AAVTnEa4vEs" />
-      <h2>{'Track B'}</h2>
-      <p>{i18next.t('subscribing_to_the_youtube_channel')}</p>
-      <ReactPlayer width={'100%'} url="https://youtu.be/J2li3qYgu9U" />
-    </div>
+    <section id={'player'} className={styles.wrapper}>
+      <h2 style={{ textAlign: 'center' }}>{t('subscribing_to_the_youtube_channel')}</h2>
+      <h3 style={{ textAlign: 'center' }}>{'Handson'}</h3>
+      <div className={styles.container}>
+        <iframe
+          src={'https://www.youtube.com/embed/wmjX6-mDsME?autoplay=1&mute=1'}
+          className={styles.player}
+          title={'VS Code Meetup YouTube channel'}
+          loading="lazy"
+        />
+      </div>
+      <h3 style={{ textAlign: 'center' }}>{'Track A'}</h3>
+      <div className={styles.container}>
+        <iframe
+          src={'https://www.youtube.com/embed/AAVTnEa4vEs?autoplay=1&mute=1'}
+          className={styles.player}
+          title={'VS Code Meetup YouTube channel'}
+          loading="lazy"
+        />
+      </div>
+      <h3 style={{ textAlign: 'center' }}>{'Track B'}</h3>
+      <div className={styles.container}>
+        <iframe
+          src={'https://www.youtube.com/embed/J2li3qYgu9U?autoplay=1&mute=1'}
+          className={styles.player}
+          title={'VS Code Meetup YouTube channel'}
+          loading="lazy"
+        />
+      </div>
+    </section>
   )
 }
